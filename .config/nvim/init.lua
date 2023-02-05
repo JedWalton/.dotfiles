fb90deab92ef2219c9aaf05c870f8d471bde5721
Jed <jedwalton98@gmail.com>
2023-02-05 19:49:27 +0000

    tmux panes in cwd

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -147,13 +147,54 @@ require('packer').startup(function(use)
     "aserowy/tmux.nvim",
     config = function() return require("tmux").setup() end
   })
-  
+
   use {
     "ThePrimeagen/refactoring.nvim",
       requires = {
           {"nvim-lua/plenary.nvim"},
           {"nvim-treesitter/nvim-treesitter"}
       }
+  }
+
+  use {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({"tidy", "-i", "-q", "-"}, body)
+            end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      })
+    end
   }
 
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
@@ -345,6 +386,7 @@ require('nvim-treesitter.configs').setup({
     'html',
     'css',
     'json',
+    'http',
     'help',
   },
   autotag = { enable = true },
@@ -633,10 +675,14 @@ vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring
 vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
 -- Extract block doesn't need visual mode
 vim.api.nvim_set_keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<CR>]], {noremap = true, silent = true, expr = false})
-vim.api.nvim_set_keymap("n", "<leader>rbf", [[ <Cmd>lua require('refactoring').refactor('Extract Block To File')<CR>]], {noremap = true, silent = true, expr = false})
+vim.api.nvim_set_keymap("n", "<leader>rB", [[ <Cmd>lua require('refactoring').refactor('Extract Block To File')<CR>]], {noremap = true, silent = true, expr = false})
 -- Inline variable can also pick up the identifier currently under the cursor without visual mode
 vim.api.nvim_set_keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
 
+-- Rest.nvim
+map('n', '<leader>R', '<Plug>RestNvim',{ noremap = true, silent = false })
+map('n', '<leader>Rp', '<Plug>RestNvimPreview',{ noremap = true, silent = false })
+map('n', '<leader>Rl', '<Plug>RestNvimLast',{ noremap = true, silent = false })
 -- AutoPairs
 local status, autopairs = pcall(require, 'nvim-autopairs')
 if not status then
