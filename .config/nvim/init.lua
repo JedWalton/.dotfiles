5538b6b39da4bbe27597a391115dfe4e3ca810d8
Jed <jedwalton98@gmail.com>
2023-02-05 12:50:27 +0000

    Wonderful configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,10 +25,8 @@ require('packer').startup(function(use)
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-
       -- Useful status updates for LSP
       'j-hui/fidget.nvim',
-
       -- Additional lua configuration, makes nvim stuff amazing
       'folke/neodev.nvim',
     },
@@ -198,27 +196,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
--- Make line numbers default
-vim.wo.number = true
-
--- Enable mouse mode
-vim.o.mouse = 'a'
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
-
--- Case insensitive searching UNLESS /C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Decrease update time
-vim.o.updatetime = 250
-
 -- Set colorscheme
-vim.o.termguicolors = true
 -- vim.o.background = "light" -- --[[ o ]]r "light" for light mode
 vim.cmd([[colorscheme tokyonight-storm]])
 -- nice transparency
@@ -234,8 +212,10 @@ require("transparent").setup({
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
-
 vim.opt.nu = true
+vim.wo.number = true
+vim.o.mouse = 'a'
+vim.o.breakindent = true
 vim.opt.relativenumber = true
 vim.opt.errorbells = false
 vim.opt.tabstop = 2
@@ -256,8 +236,11 @@ vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append('@-@')
 vim.opt.colorcolumn = '80'
 vim.opt.background= 'dark'
-
 vim.opt.cmdheight = 1
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 250
 
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
@@ -288,27 +271,18 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Set lualine as statusline
 -- See `:help lualine.txt`
 require('lualine').setup({
-  options = {
-    icons_enabled = false,
-    theme = 'tokyonight',
-    component_separators = '|',
-    section_separators = '',
+  options = { icons_enabled = false, theme = 'tokyonight', component_separators = '|', section_separators = '',
   },
 })
 
--- Enable Comment.nvim
 require('Comment').setup()
 
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
 require('indent_blankline').setup({
   -- char = '┊',
-  char = '',
+  char = '', -- See `:help indent_blankline.txt`
   show_trailing_blankline_indent = false,
 })
 
--- Gitsigns
--- See `:help gitsigns.txt`
 require('gitsigns').setup({
   signs = {
     add = { text = '+' },
@@ -336,18 +310,8 @@ require('telescope').setup({
 pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
-vim.keymap.set(
-  'n',
-  '<leader>?',
-  require('telescope.builtin').oldfiles,
-  { desc = '[?] Find recently opened files' }
-)
-vim.keymap.set(
-  'n',
-  '<leader><space>',
-  require('telescope.builtin').buffers,
-  { desc = '[ ] Find existing buffers' }
-)
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(
@@ -358,36 +322,11 @@ vim.keymap.set('n', '<leader>/', function()
   )
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set(
-  'n',
-  '<leader>sf',
-  require('telescope.builtin').find_files,
-  { desc = '[S]earch [F]iles' }
-)
-vim.keymap.set(
-  'n',
-  '<leader>sh',
-  require('telescope.builtin').help_tags,
-  { desc = '[S]earch [H]elp' }
-)
-vim.keymap.set(
-  'n',
-  '<leader>sw',
-  require('telescope.builtin').grep_string,
-  { desc = '[S]earch current [W]ord' }
-)
-vim.keymap.set(
-  'n',
-  '<leader>sg',
-  require('telescope.builtin').live_grep,
-  { desc = '[S]earch by [G]rep' }
-)
-vim.keymap.set(
-  'n',
-  '<leader>sd',
-  require('telescope.builtin').diagnostics,
-  { desc = '[S]earch [D]iagnostics' }
-)
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
@@ -469,8 +408,8 @@ require('nvim-treesitter.configs').setup({
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>dof', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>dsl', vim.diagnostic.setloclist)
 
 -- LSP settings.
 --  This function gets run when an LSP connects to a particular buffer.
@@ -496,16 +435,8 @@ local on_attach = function(_, bufnr)
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap(
-    '<leader>ds',
-    require('telescope.builtin').lsp_document_symbols,
-    '[D]ocument [S]ymbols'
-  )
-  nmap(
-    '<leader>ws',
-    require('telescope.builtin').lsp_dynamic_workspace_symbols,
-    '[W]orkspace [S]ymbols'
-  )
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -515,9 +446,7 @@ local on_attach = function(_, bufnr)
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
+  nmap('<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -589,10 +518,7 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
+    ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true,}),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -657,7 +583,7 @@ map('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = false })
 map('n', '<leader>bw', ':bw<CR>', { noremap = true, silent = false })
 
 -- Fast Save
-map('n', '<leader>ww', ':w<CR>', { noremap = true, silent = false })
+map('n', '<leader>W', ':w<CR>', { noremap = true, silent = false })
 
 -- Tabs
 -- Go to tab by number
@@ -679,7 +605,6 @@ map('n', '<leader>to', ':tabonly<CR>', { noremap = true, silent = false })
 
 -- Debugging
 -- For C/C++/Rust use CodeLLDB
---
 vim.cmd([[
 let g:vimspector_sidebar_width = 85
 let g:vimspector_bottombar_height = 15
@@ -700,11 +625,9 @@ vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring
 vim.api.nvim_set_keymap("v", "<leader>rf", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function To File')<CR>]], {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("v", "<leader>rv", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Variable')<CR>]], {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
-
 -- Extract block doesn't need visual mode
 vim.api.nvim_set_keymap("n", "<leader>rb", [[ <Cmd>lua require('refactoring').refactor('Extract Block')<CR>]], {noremap = true, silent = true, expr = false})
 vim.api.nvim_set_keymap("n", "<leader>rbf", [[ <Cmd>lua require('refactoring').refactor('Extract Block To File')<CR>]], {noremap = true, silent = true, expr = false})
-
 -- Inline variable can also pick up the identifier currently under the cursor without visual mode
 vim.api.nvim_set_keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
 
@@ -743,25 +666,12 @@ map('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = false })
 map('n', '<leader>gP', ':Git pull<CR>', { noremap = true, silent = false })
 
 -- Workspace remaps
-map(
-  'n',
-  '<leader>wc',
-  ':pwd<CR>',
-  { desc = '[W]orkspace [C]urrent', noremap = true, silent = false }
-)
-map(
-  'n',
-  '<leader>wd',
-  ':cd %:p:h<CR>:pwd<CR>',
-  { desc = '[W]orkspace [D]irectory', noremap = true, silent = false }
-)
+map( 'n', '<leader>wc', ':pwd<CR>', { desc = '[W]orkspace [C]urrent', noremap = true, silent = false })
+map( 'n', '<leader>wd', ':cd %:p:h<CR>:pwd<CR>', { desc = '[W]orkspace [D]irectory', noremap = true, silent = false })
 map('n', '<leader>wh', ':cd <CR>:pwd<CR>', { noremap = true, silent = false })
 
-
 -- ChatGPT
--- export OPENAI_API_KEY = <ChatGPT/OPENAI_KEY>
 map('n', '<leader>ai', ':ChatGPT<CR>', { noremap = true, silent = false })
-map('n', '<leader>aa', ':ChatGPTActAs<CR>', { noremap = true, silent = false })
 
 -- Install linters for these languages.
 -- ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'toml',
@@ -816,21 +726,9 @@ local function prettier()
   return {
     exe = 'prettier',
     args = {
-      '--config-precedence',
-      'prefer-file',
-      '--single-quote',
-      '--prose-wrap',
-      'always',
-      '--arrow-parens',
-      'always',
-      '--trailing-comma',
-      'all',
-      '--no-semi',
-      '--end-of-line',
-      'lf',
-      '--print-width',
-      80,
-      '--stdin-filepath',
+      '--config-precedence', 'prefer-file', '--single-quote', '--prose-wrap',
+      'always', '--arrow-parens', 'always', '--trailing-comma', 'all',
+      '--no-semi', '--end-of-line', 'lf', '--print-width', 80, '--stdin-filepath',
       vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
     },
     stdin = true,
@@ -917,17 +815,9 @@ formatter.setup({
         return {
           exe = 'stylua',
           args = {
-            '--indent-type',
-            'Spaces',
-            '--line-endings',
-            'Unix',
-            '--quote-style',
-            'AutoPreferSingle',
-            '--indent-width',
-            vim.bo.tabstop,
-            '--column-width',
-            80,
-            '-',
+            '--indent-type', 'Spaces', '--line-endings', 'Unix', '--quote-style', 
+            'AutoPreferSingle', '--indent-width', vim.bo.tabstop, '--column-width',
+            80, '-',
           },
           stdin = true,
         }
