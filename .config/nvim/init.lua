54e4a43ac927ac9b31e399ce0a7e39263f389166
Jed <jedwalton98@gmail.com>
2023-02-05 12:55:55 +0000

    better scrolling

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -549,6 +549,9 @@ cmp.setup({
 
 local map = vim.api.nvim_set_keymap
 
+-- Better scrolling
+map('n', '<C-d>', "<C-d>zz", { noremap = true, silent = false })
+map('n', '<C-u>', "<C-u>zz", { noremap = true, silent = false })
 -- Better copy and paste
 map('n', '<leader>p', '"+p', { noremap = true, silent = false })
 map('v', '<leader>p', '"+p', { noremap = true, silent = false })
