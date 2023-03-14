5b1552a1b32a10cf6fb1862502ed2412759f9238
jedbidlogix <jed@bidlogix.net>
2023-03-14 14:59:51 +0000

    copilot in tmux potential fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,11 +98,28 @@ require('packer').startup(function(use)
             enabled = true,
             auto_refresh = false,
             keymap = {
-              jump_prev = '[[',
-              jump_next = ']]',
-              accept = '<CR>',
-              refresh = 'gr',
-              open = '<C-CR>',
+              jump_prev = "[[",
+              jump_next = "]]",
+              accept = "<CR>",
+              refresh = "gr",
+              open = "<M-CR>"
+            },
+            layout = {
+              position = "bottom", -- | top | left | right
+              ratio = 0.4
+            },
+          },
+          suggestion = {
+            enabled = true,
+            auto_trigger = false,
+            debounce = 75,
+            keymap = {
+              accept = "<M-l>",
+              accept_word = false,
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+              dismiss = "<C-]>",
             },
           },
           filetypes = {
@@ -114,7 +131,7 @@ require('packer').startup(function(use)
             hgcommit = false,
             svn = false,
             cvs = false,
-            ['.'] = false,
+            ["."] = false,
           },
           copilot_node_command = 'node', -- Node.js version must be > 16.x
           server_opts_overrides = {},
