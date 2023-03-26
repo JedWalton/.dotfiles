418cdf0930489ac278e60e8b40876cc16dc263ec
Jed Walton <jedwaltondev@gmail.com>
2023-03-26 11:50:50 +0100

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -534,8 +534,11 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  clangd = {},
+  -- clangd = {},
   rust_analyzer = {},
+  jedi_language_server = {},
+  gopls = {},
+  tsserver = {},
 }
 
 -- Setup neovim lua configuration
