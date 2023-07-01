bd1b80ac1b9fd9dcc3228a3d0e9f16ed6fc93c87
Jed Walton <jedwaltondev@gmail.com>
2023-07-01 02:18:42 +0100

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -393,6 +393,14 @@ vim.keymap.set('n', '<leader>/', function()
     })
   )
 end, { desc = '[/] Fuzzily search in current buffer]' })
+vim.keymap.set('n', '<leader>?', function()
+  require('telescope.builtin').keymaps(
+    require('telescope.themes').get_dropdown({
+      winblend = 10,
+      previewer = false,
+    })
+  )
+end, { desc = '[?] keymaps?' })
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
